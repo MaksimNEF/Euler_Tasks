247a23713652b57fc9e798f1ea1eaa919b8f28d8
Maksim Nefedov <maksnefed1999@gmail.com>
2025-12-27 19:19:11 +0300

    PROBLEM 1: Multiples of 3 or 5

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# PROBLEM 1: Multiples of 3 or 5
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23 .
+#Find the sum of all the multiples of 3 or 5 below 1000.
 
+#First simple way
+start = time.time()
+summa = 0
+for k in range(1, 1000):
+    if k % 3 == 0 or k % 5 == 0:
+        summa += k
+end = time.time()
+print(f"Summ: {summa}")
+print(f"Time: {end - start:.8f} seconds")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Second difficult way
+start1 = time.time()
+summa1 = 0.5 * (333*(3+999)) + 0.5 * (199*(5+995)) - 0.5*(66*(15+990))
+end1 = time.time()
+print(f"Summ: {summa1}")
+print(f"Time: {end1 - start1:.8f} seconds")
